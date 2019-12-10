81b6103fe667ab8fd58b3b0a6af3b5112754fa80
Stranathan <ian.p.stranathan@gmail.com>
2019-12-10 08:17:02 -0800

    saving stuff

---
+++ Assets/Scripts/UI/hoverAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using DentedPixel;
+using UnityEngine;
+
+public class hoverAnimation : MonoBehaviour
+{
+    public LTRect buttonRect4;
+    void OnGUI()
+    {
+        if (GUI.Button(buttonRect4.rect, "RestartButton"))
+        {
+            Debug.Log("blah blah");
+            LeanTween.rotate(buttonRect4, 150.0f, 1.0f).setEase(LeanTweenType.easeOutElastic);
+            LeanTween.rotate(buttonRect4, 0.0f, 1.0f).setDelay(1.0f).setEase(LeanTweenType.easeOutElastic);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/levelManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class levelManager : MonoBehaviour
+{
+    public void loadALevel(int levelNum) 
+    {
+        SceneManager.LoadScene(levelNum);
+    }
+}
