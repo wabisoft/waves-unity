9af0537b04ce2d4267ed6d3664cd9f2f7e6863e3
Owen Stranathan <owenstranathan@gmail.com>
2019-12-18 10:56:41 -0800

    Capitalization

---
+++ Assets/Scripts/BoatWinCondition.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatWinCondition : MonoBehaviour
+{
+    public bool winFlag;
+
+    private Rigidbody2D rb;
+
+    public GameObject winPlatform;
+    public Vector2 winPos;
+    private Vector2 boat2WinPlatVector;
+
+    public float xThreshold;
+    public float yThreshold;
+
+    void Start()
+    {
+        //
+        xThreshold = 1f;
+        yThreshold = 1.2f * winPlatform.transform.localScale.y;
+        //
+        winFlag = false;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        winPos = new Vector2(winPlatform.transform.position.x, winPlatform.transform.position.y);
+        boat2WinPlatVector = new Vector2(winPos.x - gameObject.transform.position.x, winPos.y - gameObject.transform.position.y);
+
+        //Debug.Log("Scale in Y: " + " " + winPlatform.transform.localScale.y);
+    }
+
+    private void Update()
+    {
+        //
+        boat2WinPlatVector.x = winPos.x - gameObject.transform.position.x;
+        boat2WinPlatVector.y = winPos.y - gameObject.transform.position.y;
+
+        //
+        if (Mathf.Abs(boat2WinPlatVector.x) < xThreshold && Mathf.Abs(boat2WinPlatVector.y) < yThreshold && rb.IsSleeping())
+        {
+            winFlag = true;
+            //Debug.Log("You win dawg");
+        }
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private bool winFlag = false;
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject failMenu;
+    public GameObject nextLevelMenu;
+    public GameObject boat;
+    public GameObject rock;
+    BoatWinCondition boatWinCondScript;
+
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+        failMenu.SetActive(false);
+        nextLevelMenu.SetActive(false);
+        boatWinCondScript = boat.GetComponent<BoatWinCondition>();
+        winFlag = boatWinCondScript.winFlag;
+    }
+
+    void Update()
+    {
+        winFlag = boatWinCondScript.winFlag; // seems stupid, should only check if it changes
+
+        if (winFlag)
+        {
+            StartCoroutine(showNextLevelMenu(nextLevelMenu.GetComponent<RectTransform>()));
+        }
+
+        // CHANGE TO BE SOMETHING
+        if (rock.transform.position.y < -25 * Camera.main.orthographicSize)
+        {
+            StartCoroutine(showRetryMenu(failMenu.GetComponent<RectTransform>()));
+            //Time.timeScale = 0.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (gameIsPaused)
+            {
+                resume();
+            }
+            else
+            {
+                pause();
+            }
+        }
+    }
+
+    private IEnumerator showRetryMenu(RectTransform rt)
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.02f);
+        failMenu.SetActive(true);
+
+        int numSteps = 20;
+        float stepSize = 1f / (float)numSteps;
+        float tt = 0;
+        float easingFunction;
+
+        for (int i = 0; i < numSteps; i++)
+        {
+            easingFunction = Mathf.Pow(tt, 3f);
+            rt.localScale = new Vector3(easingFunction, easingFunction);
+            tt += stepSize;
+            yield return wait;
+        }
+        rt.localScale = new Vector3(1f, 1f);
+    }
+    private IEnumerator showNextLevelMenu(RectTransform rt)
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.01f);
+        nextLevelMenu.SetActive(true);
+
+        int numSteps = 30;
+        float stepSize = 1f / (float)numSteps;
+        float tt = 0;
+        float easingFunction;
+
+        for (int i = 0; i < numSteps; i++)
+        {
+            easingFunction = Mathf.Pow(tt, 3f);
+            rt.localScale = new Vector3(easingFunction, easingFunction);
+            tt += stepSize;
+            yield return wait;
+        }
+        rt.localScale = new Vector3(1f, 1f);
+    }
+
+
+    public void loadALevel(int levelNum)
+    {
+        SceneManager.LoadScene(levelNum);
+    }
+
+    public void pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        gameIsPaused = true;
+    }
+
+    public void resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        gameIsPaused = false;
+    }
+}
